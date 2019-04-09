2fc505d53da58732ca399094d4bd489f3d2163d2
Ramandeep1001 <47160820+Ramandeep1001@users.noreply.github.com>
2019-04-09 12:58:40 -0400

    about page

--- about.php
+++ about.php
@@ -1,1 +1,71 @@
-hi
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<style> 
+body {
+  background-image: url("straw.jpg"), url("brook1.jpg");
+  background-color: #cccccc;
+  
+}
+.a {
+  font-family: "Times New Roman", Times, serif;
+}
+
+</style>
+<body>
+
+<div class="jumbotron text-center">
+		 <h1 class="a"><a href="sig.html"style="color:#c7004c">HomeBasketOnline.ca</a></span></h1>
+
+  <p class="a">Today customers can purchase practically any item utilizing the Internet. 
+  Online nourishing and shopping for food is winding up progressively well known. 
+  The point of this paper is to show the states of online shopping for food and buyers' disposition towards purchasing nourishment by means of the Internet based. 
+  The appraisal depends on optional data sources. 
+  With online staple grocery stores there are no impediments associated with confinement and opening times, and purchasers approach an extensive scope of stores and items online over the world. </p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3><span style="color:white">There's no taste like home</span></h3>
+      <p class="a"><span style="color:white">its great taste...</span></p>
+      <p class="a"><span style="color:white">Good Things Grow in Ontario...</span></p>
+	  <p class="a"><span style="color:white"> helps you choose fresh food from close to home...</span></p>
+	  <p class="a"><span style="color:white">contributing to the fabric of our society... </span></p>
+    </div>
+    <div class="col-sm-4">
+      <h3 class="a"><span style="color:white">Market Research</span></h3>
+      <p class="a"><span style="color:white">Market research is the foundation of food website...</span></p>
+      <p class="a"><span style="color:white">Provides seasonal tips and recipes...</span></p>
+	  <p class="a"><span style="color:white">Promotes the depth, breadth and variety of Ontario foods...</span></p>
+    </div>
+    <div class="col-sm-4">
+      <h3 class="a"><span style="color:white">Retail Services and Food Service</span></h3>        
+      <p class="a"><span style="color:white">Provides promotions, marketing materials and more...</span></p>
+      <p class="a"><span style="color:white">Availability guide...</span></p>
+      <p class="a" ><span style="color:white">Nutrition guide...</span></p>
+    <p class="a"><span style="color:white">Advertising and Promotion...</span></p>
+	</div>
+	</div>
+	</div>
+	</div>
+<div class="jumbotron text-center">
+            <p class="a">Thanks for choosing us !!!</p> 
+			  <p class="a">contact us with</p> 
+		 <center><a href="https://twitter.com/?lang=en"><img src="./assets/twi.png" width="30" height="30"  alt=""></a>
+	 <a href="https://www.whatsapp.com/"><img src="./assets/what.png" width="30" height="30" alt=""></a>
+		<a href="https://www.facebook.com/Easyshopcom-1689674647730325/"><img src="face.png" width="30" height="30" alt=""></a>
+		 <a href="https://www.youtube.com/"><img src="./assets/y1.png" width="30" height="30" alt=""></a></center>
+		</i></b></p>
+		 
+ 
+</div>
+</body>
+</html>
